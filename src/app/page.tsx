a81cdc26baed01804ba53c457c3493b47839448e
Alexander <tverdokhlebalex@me.com>
2024-09-18 23:28:24 +0400

    final prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("../app/register");
+    router.push("/register");
   }, [router]);
 
   return null;
